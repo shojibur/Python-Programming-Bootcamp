523359b01ed553bcdc01a1008f5e0368648979dc
shajibur rahman (Shojib) <shojibur@gmail.com>
2016-12-05 12:12:48 +0800

    Exercise.py

---
+++ exercise01.py
@@ -0,0 +1,8 @@
+
+name = input("What is your name: ")
+#Age should be int 
+age = int(input("How old are you: "))
+#Convert it to str
+year = 2014 - age+100
+#output
+print(name + " will be 100 years old in the year " +str(year))
